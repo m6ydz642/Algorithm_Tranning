37eb5ff64cdb67b1aa55c946b92537a02e9442d4
m6ydz642 <m6ydz642@gmail.com>
2021-06-30 23:16:32 +0900

    단위테스트 추가

---
+++ 1093.UnitTest/1093.UnitTest.cpp
@@ -0,0 +1,18 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace My1093UnitTest
+{
+	TEST_CLASS(My1093UnitTest)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			// member a;
+			Assert::IsTrue(true);
+		}
+	};
+}
