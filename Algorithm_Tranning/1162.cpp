48be82b446280fe66c0333bb83bac91b71a44dce
m6ydz642 <m6ydz642@gmail.com>
2021-08-16 11:23:07 +0900

    1162문제 빌드처리 제외, 1163추가

--- Algorithm_Tranning/1162.cpp
+++ Algorithm_Tranning/1162.cpp
@@ -1,5 +1,3 @@
-
-// 태블릿으로 풀던 코드 문제
 // 1162 : 당신의 사주를 봐 드립니다 1 
 
 #include <iostream>
@@ -23,14 +21,23 @@ int main() {
     int MidleDigits = FirstDigits % 100;
     int LastDigits = MidleDigits % 10; // 마지막 자리수 확인
 
-    cout << "FirstDigits " << FirstDigits << endl; // 첫번째 자리
-    cout << "MidleDigits " << MidleDigits << endl; // 중간자리
-    cout << "LastDigits " << LastDigits << endl; // 마지막 숫자
+    //cout << "FirstDigits " << FirstDigits << endl; // 첫번째 자리
+    //cout << "MidleDigits " << MidleDigits << endl; // 중간자리
+    //cout << "LastDigits " << LastDigits << endl; // 마지막 숫자
 
     if (MidleDigits % 10 == 0) // 마지막 자리수가 0이면
         cout << "대박" << endl;
     else
         cout << "그럭저럭" << endl;
+
+   
+
+    // 또는 
+
+    if (LastDigits == 0)
+        cout << "대박2" << endl;
+    else
+        cout << "그럭저럭2" << endl;
 
     return 0;
 }
