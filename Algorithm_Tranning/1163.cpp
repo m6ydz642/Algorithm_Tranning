eef63cdf014ecfa1d60c5d86c75051188b41136b
m6ydz642 <m6ydz642@gmail.com>
2021-08-16 16:26:21 +0900

    세자리수 구해서 짝수 홀수 여부 확인

--- Algorithm_Tranning/1163.cpp
+++ Algorithm_Tranning/1163.cpp
@@ -3,6 +3,27 @@ using namespace std;
 
 int main() {
 
+    // 1163 : 당신의 사주를 봐 드립니다 2
+    int year;
+    int month;
+    int day;
+
+    cin >> year;
+    cin >> month;
+    cin >> day;
+
+    // 년도 계산
+    int number = year + month + day;
 
 
+    // 세번째 자리수 찾기
+    int Digits3 = number % 1000;
+    int quotient = Digits3 / 100;
+    cout << "세번째 자리수 " << Digits3 << endl; // 세번째 자리
+    cout << "세번째 자리수 단일 " << quotient << endl; // 세번째 자리
+
+    if (quotient % 2 == 0)
+        cout << "대박" << endl;
+    else
+        cout << "그럭저럭" << endl;
 }
