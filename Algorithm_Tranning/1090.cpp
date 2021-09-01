79f2443174c476acde241be7c9be3a3e3c33ebdc
m6ydz642 <m6ydz642@gmail.com>
2021-09-01 22:22:53 +0900

    1090 : [기초-종합] 수 나열하기2

--- Algorithm_Tranning/1090.cpp
+++ Algorithm_Tranning/1090.cpp
@@ -2,17 +2,17 @@
 using namespace std;
 int main() {
 
-    // 1089 : [기초-종합] 수 나열하기1
+    // 1090 : [기초-종합] 수 나열하기2
     {
-        int a, b, c = 0;
+        int a, r, n = 0;
         // 변수에 누적하는 누적합 방식 
         cin >> a; // 시작값 a
-        cin >> b; // 등차값 d
-        cin >> c; // 몇번만큼 
-   
+        cin >> r; // 등비값 r
+        cin >> n; // 몇번만큼 
+
         int sum = 1;
-        for (int i = 1; i < c; i++) {
-            a += b;
+        for (int i = 1; i < n; i++) {
+            a *= r;
         }
         cout << a << endl;
 
