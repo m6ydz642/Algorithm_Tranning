aa1d4f45d8807a4182cc859f4bb32461d6af88d7
m6ydz642 <m6ydz642@gmail.com>
2021-11-21 12:09:59 +0900

    클래스 형태로 변경

--- Algorithm_Tranning/main.cpp
+++ Algorithm_Tranning/main.cpp
@@ -1,12 +1,17 @@
 #include <stdio.h>
 #include <iostream>
 #include "1093.h"
+#include "Code1163.cpp"
 
 using namespace std; 
 
 
 	int main() {
-		member* mem = new member();
-		mem->CallNumber();
+		/*member* mem = new member();
+		mem->CallNumber();*/
+
+		Code1163 Code1163;
+		Code1163.GetCode1163Function();
+
 	}
 
